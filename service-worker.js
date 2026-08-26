6aa6a508f806ba66dad286643ce25f5a199834e4
sultonmusic <sales.infarmatik.tj@gmail.com>
2026-08-26 16:09:19 +0500

    Add PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'tyuzarplata-pwa-v1';
+const APP_SHELL = [
+  '/',
+  '/index.html',
+  '/index-2.html',
+  '/firebase-auth-sync.js',
+  '/pwa-install.js',
+  '/manifest.webmanifest',
+  '/app-icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        if (new URL(event.request.url).origin === self.location.origin) {
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request, { ignoreSearch: true }))
+  );
+});
